d62c48c60b345069b7746c7e1ca58da17493c4d2
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2018-10-09 11:24:28 -0300

    Atividade 1

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,8 +1,16 @@
 # Esse metodo recebe uma lista com as matriculas dos alunos
 # e retorna essa lista em ordem crescente de matriculas
 def retorna_matriculas_decrescente(alist):
+	k = 1
 	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
+		houveTrocas = False	
+		for i in xrange(len(alist)-k):
+			if alist[i] < alist[i+1]:
 				alist[i], alist[i+1] = alist[i+1], alist[i]
+				houveTrocas = True
+		k = k+1
+		if(not houveTrocas):
+			break
+	
 	return alist
+	
